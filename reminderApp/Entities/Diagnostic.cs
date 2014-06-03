1ed6f2d980d26d63296cf732484d48e4c826c2ab
Nikolai <nicholasrabota@gmail.com>
2014-06-03 23:10:55 +0300

    create entities

---
+++ reminderApp/Entities/Diagnostic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reminderApp.Entities
+    {
+    class Diagnostic
+        {
+        #region Properties
+
+        public int DiagnosticId
+            {
+            get;
+            set;
+            }
+
+        public string Title { get; set; }
+        #endregion
+        #region Constructor
+
+        public Diagnostic(int diagnosticId,string title)
+        {
+            DiagnosticId = diagnosticId;
+            Title = title;
+        }
+        #endregion
+        }
+    }
---
+++ reminderApp/Entities/Pacient.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace reminderApp.Entities
+    {
+    public class Pacient
+        {
+        #region Properties
+
+        public int PacientId
+            {
+            get;
+            set;
+            }
+        public static string Name
+            {
+            get;
+            set;
+            }
+        public string Surname
+            {
+            get;
+            set;
+            }
+        public string SecondName
+            {
+            get;
+            set;
+            }
+        public string Address
+            {
+            get;
+            set;
+            }
+        public int Age
+            {
+            get;
+            set;
+            }
+        public DateTime DateOfBirth
+            {
+            get;
+            set;
+            }
+
+        #endregion
+
+        #region Constructor
+
+        public Pacient(int pacientId, string name, string surname, string secondName, string address, int age, DateTime dateOfBirth)
+            {
+            PacientId = pacientId;
+            Name = name;
+            Surname = surname;
+            SecondName = secondName;
+            Address = address;
+            Age = age;
+            DateOfBirth = dateOfBirth;
+            }
+        #endregion
+        }
+    }
